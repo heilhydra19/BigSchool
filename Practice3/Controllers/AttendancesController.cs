5333f06197c869b19744c102d10466430271528d
heilhydra19 <58034367+heilhydra19@users.noreply.github.com>
2021-07-13 17:42:21 +0700

    Add project files.

---
+++ Practice3/Controllers/AttendancesController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.AspNet.Identity;
+using Practice3.Models;
+
+namespace Practice3.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // PUT: api/Attendances/5
+        //[ResponseType(typeof(void))]
+        //public IHttpActionResult PutAttendance(Attendance attendance)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    db.Entry(attendance).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!AttendanceExists(attendance))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
+
+        // POST: api/Attendances
+        [ResponseType(typeof(Attendance))]
+        public IHttpActionResult PostAttendance(Course course)
+        {
+            var attendance = new Attendance() { CourseId = course.Id, Attendee = User.Identity.GetUserId() };
+            db.Attendances.Add(attendance);
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (AttendanceExists(attendance))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok();
+        }
+
+        // DELETE: api/Attendances/5
+        //[ResponseType(typeof(Attendance))]
+        //public IHttpActionResult DeleteAttendance(int id)
+        //{
+        //    Attendance attendance = db.Attendances.Find(id);
+        //    if (attendance == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    db.Attendances.Remove(attendance);
+        //    db.SaveChanges();
+
+        //    return Ok(attendance);
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AttendanceExists(Attendance attendance)
+        {
+            return db.Attendances.Any(e => e.CourseId == attendance.CourseId && e.Attendee == attendance.Attendee);
+        }
+    }
+}
